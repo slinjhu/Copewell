224f167f99e7aed42b262262b32ff83761653235
Sen Lin <slin@jhu.edu>
2015-12-12 22:43:46 -0500

    Updated scaled data again.

--- scale.m
+++ scale.m
@@ -7,11 +7,21 @@ for i = 1:length(ids)
     outfilename = sprintf('data_scaled/%s.csv', id);
     
     T = readtable(filename);
-    data = T{:, 'value'};
-    data(data<0) = 0;
-    [data, lambda] = boxcox(data+eps);
-    data = (data - min(data)) / range(data);
-    T{:, 'value'} = data;
+    x = T{:, 'value'};
+    
+    % Box Cox transformation
+    x(x<0) = 0;
+    [x, lambda] = boxcox(x+eps);
+    
+    % Truncation in both ends using 3 std
+    right_target = mean(x) + 3*std(x);
+    left_target = mean(x) - 3*std(x);
+    x(x > right_target) = right_target;
+    x(x < left_target) = left_target;
+    
+    % scale to [0,1]
+    x = (x - min(x)) / range(x);
+    T{:, 'value'} = x;
     writetable(T, outfilename);
 end
 
