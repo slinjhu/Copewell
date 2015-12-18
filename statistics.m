c664ec5e41b86a1b2fb94226174ebc5765b41003
Sen Lin <slin@jhu.edu>
2015-12-18 12:30:43 -0500

    Changed data statistics to html format.

--- statistics.m
+++ statistics.m
@@ -1,30 +1,36 @@
 clear, clc, close all
 
-data_name = 'data_scaled';
-fid = fopen(sprintf('public/%s.csv', data_name), 'w');
-fprintf(fid, 'Measure, # entires, # zeros, Min, Max, Mean, Median, Std, Mean/Median, Std/Mean, Skewness\n');
-
+data_name = 'data';
+fid = fopen(sprintf('public/%s.html', data_name), 'w');
 ids = natdir([data_name, '/*.csv']);
+fprintf(fid, '<table border="1">');
+
 for i = 1:length(ids)
     id = ids{i};
-
+    if mod(i, 20) == 1
+        fprintf(fid, ['<tr><th>ID</th><th># entries</th><th># zeros</th><th>Min</th><th>Max</th>'...
+            '<th>Mean</th><th>Median</th><th>STD</th><th>Mean/Median</th><th>STD/Mean</th><th>Skewness</th><tr>']);
+        
+    end
     % load data
     filename = sprintf('%s/%s.csv', data_name, id);
     T = readtable(filename);
     data = T{:,'value'};
     
     % calculate numbers
-    fprintf(fid, '%s', id);
-    fprintf(fid, ',%d', length(data));
-    fprintf(fid, ',%d', sum(data==0));
-    fprintf(fid, ',%3g', min(data));
-    fprintf(fid, ',%3g', max(data));
-    fprintf(fid, ',%3g', mean(data));
-    fprintf(fid, ',%3g', median(data));
-    fprintf(fid, ',%1.2f', std(data));
-    fprintf(fid, ',%1.2f', mean(data) / median(data));
-    fprintf(fid, ',%1.2f', std(data) / mean(data));
-    fprintf(fid, ',%1.2f', skewness(data));
-    fprintf(fid, '\n');
+    
+    fprintf(fid, '<tr><th>%s</th>', id);
+    fprintf(fid, '<td>%d</td>', length(data));
+    fprintf(fid, '<td>%d</td>', sum(data==0));
+    fprintf(fid, '<td>%.2g</td>', min(data));
+    fprintf(fid, '<td>%.2g</td>', max(data));
+    fprintf(fid, '<td>%.2g</td>', mean(data));
+    fprintf(fid, '<td>%.2g</td>', median(data));
+    fprintf(fid, '<td>%1.2f</td>', std(data));
+    fprintf(fid, '<td>%1.2f</td>', mean(data) / median(data));
+    fprintf(fid, '<td>%1.2f</td>', std(data) / mean(data));
+    fprintf(fid, '<td>%1.2f</td>', skewness(data));
+    fprintf(fid, '</tr>\n');
     
 end
+fprintf(fid, '</table>');
